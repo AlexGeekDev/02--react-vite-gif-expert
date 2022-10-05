5a365a0dd997fe59679d9285d0bf7ae429559f72
Alex Zepeda <ing.alex.za@gmail.com>
2022-10-05 16:02:29 -0500

    Test complete
    Test complete

---
+++ tests/GifExpertApp.test.jsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import { GifExpertApp } from '../src/GifExpertApp';
+
+
+describe('Pruebas en <GifExpertApp />', () => {
+
+    test('should ', () => {
+        
+        render( <GifExpertApp /> );
+        screen.debug();
+
+
+    });
+
+
+});
